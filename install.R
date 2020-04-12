cac53ba34977b45ce72acdc9f4995162ad40cacf
Colin <44040608+CQuinn8@users.noreply.github.com>
2020-04-12 11:58:56 -0700

    Updated lib locs in install.R

--- install.R
+++ install.R
@@ -1,6 +1,10 @@
-install.packages("rlang")
-install.packages("batchtools")
-install.packages("namedCapture")
-install.packages("caret")
-install.packaged("earth")
-install.packages("WeightedROC")
+# Specify library location to change where libraries are downloaded and loaded.
+userID = # e.g. abc123
+ll = paste0("/scratch/", userID, "/R/3.5/")
+ 
+install.packages(c("rlang","batchtools","namedCapture", 
+                   "caret", "earth", 
+                   "WeightedROC"), 
+                 lib = ll)
+
+# install.packages("batchtools", lib.loc = ll)

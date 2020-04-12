6d451ff991a0e2149970a6869f1f55de68ee1505
Colin <44040608+CQuinn8@users.noreply.github.com>
2020-04-12 12:03:33 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # Specify library location to change where libraries are downloaded and loaded.
-userID = # e.g. abc123
+userID = "" # e.g. abc123
 ll = paste0("/scratch/", userID, "/R/3.5/")
  
 install.packages(c("rlang","batchtools","namedCapture", 

3b85db08882d5abfb81895089772f70f2df5f9ca
Colin <44040608+CQuinn8@users.noreply.github.com>
2020-04-12 15:56:46 -0700

    Added ranger and e1071

--- install.R
+++ install.R
@@ -1,9 +1,9 @@
 # Specify library location to change where libraries are downloaded and loaded.
 # If this is your first R session in this directory, "/R/" will not exist.
 .libPaths() # make sure [1] is your wd
-install.packages(c("rlang","batchtools","namedCapture", 
-                   "caret", "earth", 
-                   "WeightedROC"))
+install.packages(c("rlang","batchtools","namedCapture", # general packages
+                   "caret", "earth", "ranger", "e1071", # ML packages
+                   "WeightedROC")) # error assessment
 
 
 # if you have /R/ in your wd use the below
@@ -11,6 +11,6 @@ install.packages(c("rlang","batchtools","namedCapture",
 #ll = paste0("/scratch/", userID, "/R/3.5/")
 #
 #install.packages(c("rlang","batchtools","namedCapture", 
-#                   "caret", "earth", 
+#                   "caret", "earth", "ranger", "e1071",
 #                   "WeightedROC"), 
 #                 lib = ll)

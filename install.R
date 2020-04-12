f5a215b959d1f67dd40be110a317b96f79abbeaa
Colin <44040608+CQuinn8@users.noreply.github.com>
2020-04-12 13:29:26 -0700

    Cleaned up formatting

--- install.R
+++ install.R
@@ -1,10 +1,16 @@
 # Specify library location to change where libraries are downloaded and loaded.
-userID = "" # e.g. abc123
-ll = paste0("/scratch/", userID, "/R/3.5/")
- 
+# If this is your first R session in this directory, "/R/" will not exist.
+.libPaths() # make sure [1] is your wd
 install.packages(c("rlang","batchtools","namedCapture", 
                    "caret", "earth", 
-                   "WeightedROC"), 
-                 lib = ll)
+                   "WeightedROC"))
 
-# install.packages("batchtools", lib.loc = ll)
+
+# if you have /R/ in your wd use the below
+#userID = "" # e.g. abc123
+#ll = paste0("/scratch/", userID, "/R/3.5/")
+#
+#install.packages(c("rlang","batchtools","namedCapture", 
+#                   "caret", "earth", 
+#                   "WeightedROC"), 
+#                 lib = ll)

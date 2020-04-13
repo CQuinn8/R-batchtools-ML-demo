b9d2626ff972ead7f73acab7e363e2edf68fd822
Colin <44040608+CQuinn8@users.noreply.github.com>
2020-04-13 09:20:01 -0700

    Corrected relative pathway

--- batchtools.conf.R
+++ batchtools.conf.R
@@ -1,5 +1,5 @@
 (slurm.tmpl <- normalizePath(
-  "~/slurm-afterok.tmpl",
+  "./slurm-afterok.tmpl",
   mustWork=TRUE))
 cluster.functions = makeClusterFunctionsSlurm(slurm.tmpl)
 ## Uncomment for running jobs interactively rather than using SLURM:
